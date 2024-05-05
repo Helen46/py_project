d1e7717fb56d028071e0fb63acb463628a710c7c
Елена Прилуцкая <a.priluckay87@gmail.com>
2024-05-05 22:35:55 +0300

    add a new func in funcs.py

--- funcs.py
+++ funcs.py
@@ -1,5 +1,12 @@
+from operator import sub
+
+
 def sum_of_numbers(*args):
     return sum(args)
+
+
+def difference_of_numbers(num_1, num_2):
+    return sub(num_1, num_2)
 
 
 # result = sum_of_numbers(858, 768)
